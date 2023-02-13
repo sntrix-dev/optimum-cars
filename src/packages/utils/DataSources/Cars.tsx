ca4ddc9bb3308fe01e9da8805a8d803f7bdea8b8
surya-itday <surya@itday.in>
2023-02-13 23:49:13 +0530

    solved build issues

--- src/packages/utils/DataSources/Cars.tsx
+++ src/packages/utils/DataSources/Cars.tsx
@@ -1,57 +1,61 @@
-import { Children, cloneElement, FC, HTMLAttributes, isValidElement, useEffect, useState } from "react";
+import {
+  Children,
+  cloneElement,
+  FC,
+  HTMLAttributes,
+  isValidElement,
+  useEffect,
+  useState,
+} from "react";
 import { useSearchParams } from "react-router-dom";
 import { Cars, carsData } from "../../data";
 import { Condition } from "../Components";
 
 interface CarsSourcerProps extends HTMLAttributes<HTMLDivElement> {
-    children: JSX.Element
+  children: JSX.Element;
 }
 
-export const CarsSourcer: FC<CarsSourcerProps> = ({
-    children,
-    ...props
-}) => {
+export const CarsSourcer: FC<CarsSourcerProps> = ({ children, ...props }) => {
+  const [cars, setCars] = useState<Cars[]>(carsData);
 
-    const [cars, setCars] = useState<Cars[]>(carsData)
+  const [searchParams] = useSearchParams();
 
-    const [searchParams, setSearchParams] = useSearchParams()
+  const search = searchParams.get("search");
 
-    const search = searchParams.get('search');
-    const production_year = searchParams.get('production_year');
-    const car_type = searchParams.get('car_type');
+  useEffect(() => {
+    if ((search?.length ?? false) > 0) {
+      const newCars = cars.filter((car) => {
+        const title = car.title
+          ?.toLowerCase()
+          .replaceAll(" ", "")
+          .slice(0, (search?.length ?? 1) - 1);
+        console.log(title, search?.toLowerCase().replaceAll(" ", ""));
+        return car;
+      });
 
-    useEffect(() => {
-        if ((search?.length ?? false) > 0) {
+      setCars(newCars);
+    }
+  }, [search]);
 
-            const newCars = cars.filter(car => {
-                const title = car.title?.toLowerCase().replaceAll(" ", "").slice(0, ((search?.length ?? 1) - 1))
-                console.log(title, search?.toLowerCase().replaceAll(" ", ""));
-
-            });
-
-            setCars(newCars)
-        }
-    }, [search])
-
-    return (
-        <>
-            <Condition {...props} condition={cars.length > 0} >
-                {cars.map((car, index) => {
-                    return (
-                        <div key={index}>
-                            {Children.map(children, (child) => {
-                                if (isValidElement(child)) {
-                                    return cloneElement(child, {
-                                        ...car
-                                    })
-                                } else {
-                                    return <></>
-                                }
-                            })}
-                        </div>
-                    )
-                })}
-            </Condition>
-        </>
-    )
-}
+  return (
+    <>
+      <Condition {...props} condition={cars.length > 0}>
+        {cars.map((car, index) => {
+          return (
+            <div key={index}>
+              {Children.map(children, (child) => {
+                if (isValidElement(child)) {
+                  return cloneElement(child, {
+                    ...car,
+                  });
+                } else {
+                  return <></>;
+                }
+              })}
+            </div>
+          );
+        })}
+      </Condition>
+    </>
+  );
+};
--- src/packages/utils/DataSources/Filter.tsx
+++ src/packages/utils/DataSources/Filter.tsx
@@ -1,57 +1,63 @@
-import { Children, cloneElement, FC, HTMLAttributes, isValidElement, useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import {
+  Children,
+  cloneElement,
+  FC,
+  HTMLAttributes,
+  isValidElement,
+  useEffect,
+  useState,
+} from "react";
 import { Cars, carsData } from "../../data";
 import { Condition } from "../Components";
 
 interface FilterSourcerProps extends HTMLAttributes<HTMLDivElement> {
-    children: JSX.Element
-    filterBy: "start_production" | "car_type"
+  children: JSX.Element;
+  filterBy: "start_production" | "car_type";
 }
 
 export const FilterSourcer: FC<FilterSourcerProps> = ({
-    children,
-    filterBy,
-    ...props
+  children,
+  filterBy,
+  ...props
 }) => {
+  const [filter, setFilter] = useState<string[]>([]);
 
-    const [filter, setFilter] = useState<string[]>([]);
+  useEffect(() => {
+    carsData.map((car: Cars) => {
+      const carFilter = car[filterBy]?.toString();
+      if (!!carFilter) {
+        setFilter((prevState: string[]) => {
+          if (!prevState.includes(carFilter)) {
+            return [...prevState, carFilter];
+          } else {
+            return prevState;
+          }
+        });
+      }
+      return car;
+    });
+  }, [filterBy]);
 
-    useEffect(() => {
-        carsData.map((car: Cars) => {
-            const carFilter = car[filterBy]?.toString()
-            if (!!carFilter) {
-                setFilter((prevState: string[]) => {
-                    if (!prevState.includes(carFilter)) {
-                        return [...prevState, carFilter]
-                    } else {
-                        return prevState
-                    }
-                })
-            }
-        })
-    }, [filterBy])
-
-
-    return (
-        <>
-            <Condition {...props} condition={carsData.length > 0} >
-                {filter.map((f, index) => {
-                    return (
-                        <div key={index}>
-                            {Children.map(children, (child) => {
-                                if (isValidElement(child)) {
-                                    return cloneElement<any>(child, {
-                                        filterValue: f,
-                                        query: filterBy
-                                    })
-                                } else {
-                                    return <></>
-                                }
-                            })}
-                        </div>
-                    )
-                })}
-            </Condition>
-        </>
-    )
-}
+  return (
+    <>
+      <Condition {...props} condition={carsData.length > 0}>
+        {filter.map((f, index) => {
+          return (
+            <div key={index}>
+              {Children.map(children, (child) => {
+                if (isValidElement(child)) {
+                  return cloneElement<any>(child, {
+                    filterValue: f,
+                    query: filterBy,
+                  });
+                } else {
+                  return <></>;
+                }
+              })}
+            </div>
+          );
+        })}
+      </Condition>
+    </>
+  );
+};
